377ba597b1a3efe501cb920cc226f605d192f8bf
Ayush Agarwal <agarwalayush2396@gmail.com>
2024-10-10 13:59:11 +0100

    committing basic sqlite db

---
+++ psd/python/sqlite-intro.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+# with sqlite3.connect("compay.db") as db:
+#     cursor = db.cursor()
+#     cursor.execute("""
+#         CREATE TABLE IF NOT EXISTS students(
+#             id INTEGER PRIMARY KEY,
+#             name TEXT NOT NULL,
+#             class TEXT NOT NULL,
+#             grade INTEGER
+#         );
+#     """)
+    # cursor.execute("""
+    #     INSERT INTO students(id, name, class, grade) VALUES
+    #     (123, 'Mary', 'Python', 8)
+    # """)
+    # db.commit()
+    # cursor.execute("SELECT * FROM students;")
+    # print(cursor.fetchall())
+    # db.close()
+
+conn = sqlite3.connect('compay.db')
+new_cursor = conn.cursor()
+newId = input("Enter ID : ")
+name = input("Enter name : ")
+newclass = input("Enter class : ")
+newgrade = input("Enter grade : ")
+new_cursor.execute("""
+    INSERT INTO students VALUES(?,?,?,?)
+""", (newId, name, newclass, newgrade))
+conn.commit()
+new_cursor.execute("SELECT * FROM students;")
+for x in new_cursor.fetchall():
+    print(x)
+conn.close()
